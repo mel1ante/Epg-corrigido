c60ec62bc1432d656e84f3aab815687778657b44
mel1ante <bertomoreira@msn.com>
2025-06-16 22:22:02 +0100

    Update ajustar_epg.py

--- ajustar_epg.py
+++ ajustar_epg.py
@@ -1,28 +1,23 @@
-import gzip
 import xml.etree.ElementTree as ET
 from datetime import datetime, timedelta
-import urllib.request
+import gzip
 
-# Download do EPG original
-url = 'https://epgshare01.online/epgshare01/epg_ripper_PT1.xml.gz'
-urllib.request.urlretrieve(url, 'epg.xml.gz')
+# Descomprimir o ficheiro original
+with gzip.open("epg_ripper_PT1.xml.gz", "rb") as f:
+    tree = ET.parse(f)
 
-# Descomprimir
-with gzip.open('epg.xml.gz', 'rb') as f_in:
-    xml_data = f_in.read()
+root = tree.getroot()
 
-# Parse XML
-root = ET.fromstring(xml_data)
-
-def ajustar_data(data_str):
+# Função para somar 1 minuto ao formato de tempo EPG
+def ajustar_tempo(data_str):
     dt = datetime.strptime(data_str, "%Y%m%d%H%M%S %z")
     dt += timedelta(minutes=1)
     return dt.strftime("%Y%m%d%H%M%S %z")
 
-# Ajustar horários
-for prog in root.findall('programme'):
-    prog.set('start', ajustar_data(prog.get('start')))
-    prog.set('stop', ajustar_data(prog.get('stop')))
+# Ajustar horários dos elementos <programme>
+for prog in root.findall("programme"):
+    prog.attrib["start"] = ajustar_tempo(prog.attrib["start"])
+    prog.attrib["stop"] = ajustar_tempo(prog.attrib["stop"])
 
-# Guardar XML corrigido
-ET.ElementTree(root).write("epg_corrigido.xml", encoding="utf-8", xml_declaration=True)
+# Guardar o novo ficheiro corrigido
+tree.write("epg_corrigido.xml", encoding="utf-8", xml_declaration=True)
