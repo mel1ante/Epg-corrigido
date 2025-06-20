2f3a44da93bce08b65e9250dcbfee0f4ff356d63
mel1ante <bertomoreira@msn.com>
2025-06-20 15:40:15 +0100

    Update ajustar_epg.py

--- ajustar_epg.py
+++ ajustar_epg.py
@@ -1,6 +1,13 @@
+import requests
 import xml.etree.ElementTree as ET
 from datetime import datetime, timedelta
 import gzip
+
+# Fazer download do ficheiro original
+url = "https://epgshare01.online/epgshare01/epg_ripper_PT1.xml.gz"
+r = requests.get(url)
+with open("epg_ripper_PT1.xml.gz", "wb") as f:
+    f.write(r.content)
 
 # Descomprimir o ficheiro original
 with gzip.open("epg_ripper_PT1.xml.gz", "rb") as f:
